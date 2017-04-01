6974bbae1e189227d18f8c982bdbd59d66267f30
Salvatore Tosti <saltosti@gmail.com>
2017-04-01 08:26:13 -0700

    Refactored DecorativeRotation
    Script is now significantly simpler, cut out lots of cruft.
    Also, added rotation dampening as angle of rotation increases.

--- league/Assets/Scripts/DecorativeRotation.cs
+++ league/Assets/Scripts/DecorativeRotation.cs
@@ -1,36 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DecorativeRotation : MonoBehaviour {
+public class DecorativeRotation : MonoBehaviour
+{
 	public Rigidbody rigidBody;
-	public GameObject carFront;
-	public GameObject carBack;
-	private Vector3 turnVector;
 	private float turnInput;
-	public float rotationMulu;
-	// Use this for initialization
-	void Start () {
-		turnVector = carFront.transform.position - carFront.transform.position;
-		turnInput = Input.GetAxis ("Horizontal");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		turnInput = Input.GetAxis ("Horizontal");
-		turnVector = carFront.transform.position - carBack.transform.position;
-		Debug.DrawRay (carFront.transform.position, turnVector*turnInput, Color.blue);
-		Vector3 rotationForce = turnVector * turnInput;
+	public float tiltSpeed = 30.0f;
 
-		float tiltAngle = 30.0f * turnInput;
-		float smooth = 2f;
-		Quaternion target = Quaternion.Euler (0, 0, -1*tiltAngle);
-		Transform objectTransform = GetComponent<Transform> ();
-		objectTransform.rotation = Quaternion.Slerp (objectTransform.rotation,target,Time.deltaTime *smooth);
-		//
-		//objectTransform.Rotate (rotationForce*rotationMulu);
-		//rigidBody.AddTorque (rotationForce, ForceMode.Acceleration);
-	}
-	void FixedUpdate(){
+	private float initialThreshold = 0.1f;
+	private float maxThreshold = 0.2f;
 
+	void Update ()
+	{
+		if (Input.GetAxisRaw ("Horizontal") == 0) {
+			RotateTowardLocalZero ();
+			return;
+		}
+
+		float tiltAngle = tiltSpeed * Input.GetAxisRaw ("Horizontal");
+		float localRotationZ = transform.localRotation.z;
+		float absoluteLocalRotationZ = Mathf.Abs (localRotationZ);
+
+		if (absoluteLocalRotationZ >= 0 && absoluteLocalRotationZ < initialThreshold) {
+			transform.Rotate (0, 0, -1 * tiltAngle * Time.deltaTime);
+		} else if (absoluteLocalRotationZ < maxThreshold) {
+			//smooths out end of rotation
+			float rotationPercentage = (maxThreshold - absoluteLocalRotationZ) / initialThreshold;
+			Debug.Log ("rotation Percentage: "+ rotationPercentage);
+			transform.Rotate (0, 0, -1 * tiltAngle * Time.deltaTime * rotationPercentage);
+		}
+	}
+
+	private void RotateTowardLocalZero ()
+	{
+		float smoothTiltSpeed = 2.0f;
+		transform.localRotation = Quaternion.Slerp (transform.localRotation, Quaternion.Euler (0, 0, 0), Time.deltaTime * smoothTiltSpeed);
 	}
 }
